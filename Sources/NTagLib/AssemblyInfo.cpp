d942a4b43bd46e71e323e8f967a2483d78c1dcde
Cyber Sinh <cyber.sinh@free.fr>
2020-01-01 12:03:14 +0100

    2020 is the new year

--- Sources/NTagLib/AssemblyInfo.cpp
+++ Sources/NTagLib/AssemblyInfo.cpp
@@ -6,31 +6,14 @@ using namespace System::Runtime::CompilerServices;
 using namespace System::Runtime::InteropServices;
 using namespace System::Security::Permissions;
 
-//
-// Les informations générales relatives à un assembly dépendent de
-// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
-// associées à un assembly.
-//
 [assembly:AssemblyTitleAttribute("NTagLib")];
 [assembly:AssemblyDescriptionAttribute("A C++/CLI assembly for tagging audio files with TagLib in managed code.")]
 [assembly:AssemblyConfigurationAttribute("")];
 [assembly:AssemblyCompanyAttribute("Luminescence Software")];
 [assembly:AssemblyProductAttribute("NTagLib")];
-[assembly:AssemblyCopyrightAttribute("Sylvain Rougeaux, © 2019")];
+[assembly:AssemblyCopyrightAttribute("Sylvain Rougeaux, © 2019-2020")];
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
-
-//
-// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
-//
-//      Version principale
-//      Version secondaire
-//      Numéro de build
-//      Révision
-//
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de révision et de build par défaut
-// en utilisant '*', comme indiqué ci-dessous :
-
 [assembly:AssemblyVersionAttribute("1.0.0.0")];
 [assembly:ComVisible(false)];
 [assembly:CLSCompliantAttribute(true)];
