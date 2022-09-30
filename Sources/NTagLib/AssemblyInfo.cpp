13733f0b63e07c16749d245fbe63d9c26553ac91
Cyber Sinh <cyber.sinh@free.fr>
2022-09-30 23:22:01 +0200

    Complete TagNameKey class

--- Sources/NTagLib/AssemblyInfo.cpp
+++ Sources/NTagLib/AssemblyInfo.cpp
@@ -7,13 +7,13 @@ using namespace System::Runtime::InteropServices;
 using namespace System::Security::Permissions;
 
 [assembly:AssemblyTitle("NTagLib")];
-[assembly:AssemblyDescription("A C++/CLI assembly for tagging audio files with TagLib (commit 0470c28) in managed code.")]
+[assembly:AssemblyDescription("A C++/CLI assembly for tagging audio files with TagLib in managed code.")]
 [assembly:AssemblyConfiguration("")];
-[assembly:AssemblyCompany("Luminescence Software")];
+[assembly:AssemblyCompany("Sylvain Rougeaux")];
 [assembly:AssemblyProduct("NTagLib")];
 [assembly:AssemblyCopyright("Sylvain Rougeaux, © 2019-2022")];
 [assembly:AssemblyTrademark("")];
 [assembly:AssemblyCulture("")];
-[assembly:AssemblyVersion("1.12.2.0")]; // Major.Minor.x.x is the version number of taglib, x.x.Build.Revision is the version number of NTagLib
+[assembly:AssemblyVersion("1.13.0.0")]; // Major.Minor.x.x is the version number of taglib, x.x.Build.Revision is the version number of NTagLib
 [assembly:ComVisible(false)];
 [assembly:CLSCompliant(true)];
