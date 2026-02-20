b0fe766d72e3827cd2cd08b80ac0e86151f4ce35
Cyber Sinh <cyber.sinh@free.fr>
2026-02-20 18:02:56 +0100

    Version 3.1.0.0, using Taglib 2.2

--- Sources/NTagLib/AssemblyInfo.cpp
+++ Sources/NTagLib/AssemblyInfo.cpp
@@ -17,5 +17,5 @@ using namespace System::Security::Permissions;
 [assembly:ComVisible(false)];
 [assembly:CLSCompliant(true)];
 
-[assembly:AssemblyVersion("3.0.6.0")];
+[assembly:AssemblyVersion("3.1.0.0")];
 
